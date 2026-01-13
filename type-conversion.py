bb2626e2fd54ef59e76e7364c69b20f61955c89f
SumukhMore <sumukhmore@gmail.com>
2026-01-13 14:31:30 +0530

    Type-Conversion

---
+++ type-conversion.py
@@ -0,0 +1,31 @@
+#type converion
+
+# int to float
+a = 5
+b = float(a)
+print(b)          #5.0
+
+# float to int
+c = 5.7
+d = int(c)
+print(d)          #5
+
+# int to str
+e = 10
+f = str(e)
+print(f)          #'10'
+
+# str to int
+g = "20"
+h = int(g)
+print(h)          #20
+
+# str to float
+i = "15.5"
+j = float(i)
+print(j)          #15.5
+
+# float to str
+k = 9.8
+l = str(k)
+print(l)          #'9.8'
