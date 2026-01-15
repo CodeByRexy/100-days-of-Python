3f89ad623ac6e85ed2e15108b4a68851c704ea3e
SumukhMore <sumukhmore@gmail.com>
2026-01-15 16:58:56 +0530

    Slicing-list

---
+++ slicing-list.py
@@ -0,0 +1,6 @@
+marks = [56, 65, 24, 42, 89] 
+print(marks[1:4]) # Slicing from index 1 to 3 
+print(marks[:3]) # Slicing from start to index 2 
+print(marks[2:]) # Slicing from index 2 to end 
+print(marks[-3:]) # Slicing the last three elements 
+print(marks[:-2]) # Slicing from start to the third last element
