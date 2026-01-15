ac5cd3c1d4628874da965f126708215775ddb0f1
SumukhMore <sumukhmore@gmail.com>
2026-01-15 17:03:30 +0530

    List-method

---
+++ list-methods.py
@@ -0,0 +1,16 @@
+list = [2, 4, 5, 6, 3, 1]
+list.append(7)  # Adds 7 at the end of the list
+print("After append:", list)
+
+list.sort()  # Sorts the list in ascending order
+print("After sort:", list)
+
+list.reverse()  # Reverses the order of the list
+print("After reverse:", list)
+
+list.insert(3, 9)  # Inserts 9 at index 3
+print("After insert at index 3:", list)
+
+list.remove(1)  # Removes the first occurrence of 1
+print("After remove 1:", list)
+
