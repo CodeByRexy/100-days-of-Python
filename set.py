4fb01b0bac495f96c7f00222d57c8cc2cceb3922
SumukhMore <sumukhmore@gmail.com>
2026-01-17 12:48:15 +0530

    Set

---
+++ set.py
@@ -0,0 +1,14 @@
+collection1 = {1, 2, 3, 4, 5} # set with unique elements
+
+collection2 = {1, 2, 2, 2} # duplicates will be ignored
+
+collection3 = set() # empty set syntax
+
+print(collection1)
+print(type(collection1))
+
+print(collection2)
+print(type(collection2)) 
+
+print(collection3)
+print(type(collection3))
